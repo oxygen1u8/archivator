460b860e29ef83ca6e755477f33b67a27b228274
oxygen1u8 <oxygen1u8@mail.ru>
2022-04-13 21:46:35 +0300

    archivator: add struct symbol_t and union code

--- types.h
+++ types.h
@@ -1,4 +1,31 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#define ARRAY_SIZE(x) sizeof((x))/sizeof((x[0]))
+
+typedef struct _symbol {
+    unsigned char sym;
+    char code[255];
+    float freq;
+    struct _symbol* left;
+    struct _symbol* right;
+} symbol_t;
+
+union code
+{
+    unsigned char sym_to_write;
+    struct byte
+    {
+        unsigned b0: 1;
+        unsigned b1: 1;
+        unsigned b2: 1;
+        unsigned b3: 1;
+        unsigned b4: 1;
+        unsigned b5: 1;
+        unsigned b6: 1;
+        unsigned b7: 1;
+    } byte;
+};
+
+
 #endif

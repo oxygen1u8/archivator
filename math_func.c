2dbe0a653374cc337deb6ad18459a43a8911cbff
oxygen1u8 <oxygen1u8@mail.ru>
2022-04-22 22:34:46 +0300

    archivator: added implementation of entropy function

--- math_func.c
+++ math_func.c
@@ -1,1 +1,9 @@
 #include "math_func.h"
+
+double entropy(symbol_t* symbols, int* syms_count)
+{
+    float H = 0.0f;
+    for(int i = 0; i < *syms_count; i++)
+        H += symbols[i].freq * log2(symbols[i].freq);
+    return -H;
+}

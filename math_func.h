4e2d7d0a63a7677aff232946f433c2804cac20b1
oxygen1u8 <oxygen1u8@mail.ru>
2022-04-22 22:34:06 +0300

    archivator: added function declarations

--- math_func.h
+++ math_func.h
@@ -1,4 +1,10 @@
 #ifndef MATH_FUNC_H
 #define MATH_FUNC_H
 
+#include "types.h"
+#include <math.h>
+
+double entropy_count(symbol_t* syms, int* syms_count);
+double bcost(int *fsize_2, double file_length);
+
 #endif

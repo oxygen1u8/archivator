b240462bbd38ccfdd7ced258096c55a15356e31a
oxygen1u8 <oxygen1u8@mail.ru>
2022-04-22 22:28:28 +0300

    archivator: added function reading_from_file

--- file_in_out.c
+++ file_in_out.c
@@ -1,1 +1,53 @@
 #include "file_in_out.h"
+
+void reading_from_file(FILE* fp, symbol_t** syms, int** syms_count, int* elem_count)
+{
+    union code bcode;
+    bool isExist;
+    int _elem_count = 0;
+    char syms_buff[256];
+
+    symbol_t* tmp_syms = NULL;
+    int* tmp_syms_count = NULL;
+    
+    while(!feof(fp))
+    {
+        isExist = false;
+        fread(&bcode.sym_to_write, 1, sizeof(unsigned char), fp);
+        if(tmp_syms == NULL)
+        {
+            tmp_syms = (symbol_t*) malloc(sizeof(symbol_t));
+            tmp_syms_count = (int*) malloc(sizeof(int));
+            tmp_syms[0].sym = bcode.sym_to_write;
+            tmp_syms_count[0] = 1;
+            _elem_count = 1;
+        }
+        else
+        {
+            for(int i = 0; i < _elem_count; i++)
+            {
+                if(tmp_syms[i].sym == bcode.sym_to_write)
+                {
+                    isExist = true;
+                    tmp_syms_count[i] += 1;
+                    break;
+                }
+            }
+            if(!isExist)
+            {
+                tmp_syms = (symbol_t*) realloc(tmp_syms, (_elem_count + 1) * sizeof(symbol_t));
+                tmp_syms_count = (int*) realloc(tmp_syms_count, (_elem_count + 1) * sizeof(int));
+                tmp_syms[_elem_count].sym = bcode.sym_to_write;
+                tmp_syms_count[_elem_count] = 1;
+                _elem_count += 1;
+            }
+        }
+    }
+
+    *syms = (symbol_t*) malloc(sizeof(tmp_syms));
+    *syms_count = (int*) malloc(sizeof(tmp_syms_count));
+    
+    *elem_count = _elem_count;
+    *syms = tmp_syms;
+    *syms_count = tmp_syms_count;
+}
--- file_in_out.h
+++ file_in_out.h
@@ -1,4 +1,12 @@
 #ifndef FILE_IN_OUT_H
 #define FILE_IN_OUT_H
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "types.h"
+
+void reading_from_file(FILE* fp, symbol_t** syms, int** syms_count, int* elem_count);
+void writing_to_file();
+
 #endif

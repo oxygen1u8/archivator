5980d8aeebd79dbe9e0ca21342ef827670df298d
oxygen1u8 <oxygen1u8@mail.ru>
2022-04-23 08:10:53 +0300

    archivator: added calculation of character probabilities and sorting in descending order

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+#include "information.h"
 #include "file_in_out.h"
 #include "types.h"
 
 int main(int argc, char* argv[])
 {
-    FILE* fp = fopen(argv[1], "rb");
     if(argc == 1)
     {
         fprintf(stderr, "A few arguments!\n");
         return -1;
     }
+    
+    FILE* fp = fopen(argv[1], "rb");
     if(fp == NULL)
     {
         fprintf(stderr, "File \"%s\" doesn't exist!\n",  argv[1]);
@@ -24,25 +27,49 @@ int main(int argc, char* argv[])
 
     reading_from_file(fp, &syms, &syms_count, &elem_count);
     fclose(fp);
-    
-    if(syms != NULL)
-    {
-        for(int i = 0; i < elem_count; i++)
-        {
-            printf("%c - %d\n", syms[i].sym, syms_count[i]);
-        }
-    }
-    else
+
+    if(syms == NULL)
     {
         fprintf(stderr, "File is empty!\n");
         return -3;
     }
 
-    if(syms != NULL)
+    int sum_count = 0;
+
+    for(int i = 0; i < elem_count; i++)
     {
-        free(syms);
-        free(syms_count);
+        sum_count += syms_count[i];
     }
+
+    for(int i = 0; i < elem_count; i++)
+    {
+        syms[i].freq = (float)syms_count[i] / sum_count;
+    }
+
+    symbol_t tmp;
+    int tmp_i;
+
+    for(int i = 0; i < elem_count; i++)
+    {
+        for(int j = 0; j < elem_count - 1; j++)
+        {
+            if(syms_count[j] < syms_count[j + 1])
+            {
+                tmp_i = syms_count[j + 1];
+                syms_count[j + 1] = syms_count[j];
+                syms_count[j] = tmp_i;
+
+                tmp = syms[j + 1];
+                syms[j + 1] = syms[j];
+                syms[j] = tmp;
+            }
+        }
+    }
+
+    set_information(syms, &elem_count, syms_count);
+
+    free(syms);
+    free(syms_count);
 
     return 0;
 }

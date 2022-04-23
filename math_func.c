e4ad803d310439d06aaef80c61fd8476796644dd
oxygen1u8 <oxygen1u8@mail.ru>
2022-04-23 08:09:40 +0300

    archivator: fixed title of entropy_count function

--- math_func.c
+++ math_func.c
@@ -1,9 +1,14 @@
 #include "math_func.h"
 
-double entropy(symbol_t* symbols, int* syms_count)
+double entropy_count(symbol_t* symbols, int* syms_count)
 {
-    float H = 0.0f;
+    double H = 0.0;
     for(int i = 0; i < *syms_count; i++)
         H += symbols[i].freq * log2(symbols[i].freq);
     return -H;
+}
+
+double bcost(int *fsize_2, double file_length)
+{
+    
 }

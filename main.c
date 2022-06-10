1b16068ad914d72738de6cea605b15d03de54fd0
oxygen1u8 <oxygen1u8@mail.ru>
2022-06-10 16:57:41 +0300

    changed main file

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #include "information.h"
 #include "file_in_out.h"
@@ -9,15 +10,13 @@
 
 int main(int argc, char* argv[])
 {
-    if(argc == 1)
-    {
+    if(argc == 1) {
         fprintf(stderr, "A few arguments!\n");
         return -1;
     }
     
     FILE* fp = fopen(argv[1], "rb");
-    if(fp == NULL)
-    {
+    if(fp == NULL) {
         fprintf(stderr, "File \"%s\" doesn't exist!\n",  argv[1]);
         return -2;
     }
@@ -29,36 +28,45 @@ int main(int argc, char* argv[])
     reading_from_file(fp, &syms, &syms_count, &elem_count);
     fclose(fp);
 
-    if(syms == NULL)
-    {
+    if(syms == NULL) {
         fprintf(stderr, "File is empty!\n");
         return -3;
     }
 
     int sum_count = 0;
 
-    for(int i = 0; i < elem_count; i++)
-    {
+    for(int i = 0; i < elem_count; i++) {
         sum_count += syms_count[i];
     }
 
-    for(int i = 0; i < elem_count; i++)
-    {
+    for(int i = 0; i < elem_count; i++) {
         syms[i].freq = (float)syms_count[i] / sum_count;
     }
 
     free(syms_count);
 
-    symbol_t tmp;
-    int tmp_i;
-
-    syms = sort_syms(syms, &elem_count);
     symbol_t* tree = make_tree(syms, elem_count);
 
+    make_codes(&tree, &syms, &elem_count);
+    for(uint32_t i = 0; i < elem_count; i++) {
+        strcat(syms[i].code, "\0");
+    }
+
+    fp = fopen(argv[1], "rb");
+    char archiveNameTemp[256];
+    char archiveName[256];
+    strcpy(archiveNameTemp, argv[1]);
+    strcat(archiveNameTemp, ".atemp");
+    FILE *temp = fopen(archiveNameTemp, "rwb");
+    FILE *dest = fopen(archiveName, "wb");
+
+    writing_to_file(fp, temp, dest, &syms, &elem_count);
+    fclose(fp);
+    fclose(temp);
+    fclose(dest);
     set_information(syms, &elem_count);
 
     free(syms);
-    
 
     return 0;
 }
